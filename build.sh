5b86997e3fabbaf3889af3db2a38024f2f9f3448
Julio Rincon <jcrinconc@gmail.com>
2017-03-24 22:28:16 +1100

    fix deploy

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 type lein || {
-    echo "downloading lein :)"
+    echo "downloading lein ... $HOME"
+    type wget
     wget -v -S -O ~/bin/lein "https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein"
     chmod +x ~/bin/lein
 }
