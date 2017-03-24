5a69c96715cc6cbb69d7c99418a98a451282222d
Julio Rincon <jcrinconc@gmail.com>
2017-03-24 22:32:25 +1100

    fix deploy

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 type lein || {
     echo "downloading lein ... $HOME"
     type wget
-    wget -v -S -O ~/bin/lein "https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein"
-    chmod +x ~/bin/lein
+    wget -v -S -O ~/lein "https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein"
+    chmod +x ~/lein
 }
 
-~/bin/lein uberjar
+~/lein uberjar
